d84f1c199adbeea2171e2be9534f5b0480632a92
Sébastien Pomi <sebastien.pomi@he-arc.ch>
2016-10-20 09:20:31 +0200

    add point 16

--- src/java/ch/hearc/ig/odi/peoplelist/presentation/beans/PeopleListBean.java
+++ src/java/ch/hearc/ig/odi/peoplelist/presentation/beans/PeopleListBean.java
@@ -5,8 +5,11 @@
  */
 package ch.hearc.ig.odi.peoplelist.presentation.beans;
 
+import ch.hearc.ig.odi.peoplelist.service.Services;
+import java.util.List;
 import javax.inject.Named;
 import javax.faces.bean.RequestScoped;
+import javax.inject.Inject;
 
 /**
  *
@@ -15,11 +18,16 @@ import javax.faces.bean.RequestScoped;
 @Named(value = "peopleListBean")
 @RequestScoped
 public class PeopleListBean {
-
+    
+    @Inject Services services;
     /**
      * Creates a new instance of PeopleListBean
      */
     public PeopleListBean() {
+    }
+    
+    public List getPeopleList(){
+        return services.getPeopleList();
     }
     
 }
