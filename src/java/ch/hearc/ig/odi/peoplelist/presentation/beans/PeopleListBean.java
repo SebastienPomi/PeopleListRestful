efd7ec5fdd9bb53cf32e5d695337338ddc148f46
Sébastien Pomi <sebastien.pomi@he-arc.ch>
2016-10-26 09:37:17 +0200

    probleme affichage

--- src/java/ch/hearc/ig/odi/peoplelist/presentation/beans/PeopleListBean.java
+++ src/java/ch/hearc/ig/odi/peoplelist/presentation/beans/PeopleListBean.java
@@ -5,7 +5,9 @@
  */
 package ch.hearc.ig.odi.peoplelist.presentation.beans;
 
+import ch.hearc.ig.odi.peoplelist.business.Person;
 import ch.hearc.ig.odi.peoplelist.service.Services;
+import java.util.ArrayList;
 import java.util.List;
 import javax.inject.Named;
 import javax.faces.bean.RequestScoped;
@@ -24,9 +26,11 @@ public class PeopleListBean {
      * Creates a new instance of PeopleListBean
      */
     public PeopleListBean() {
+        
     }
     
     public List getPeopleList(){
+        
         return services.getPeopleList();
     }
     
